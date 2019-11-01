235e0a2fd7534cef31b4503a8cccae69d34b9fa6
Michael Nunes <miketnunes@gmail.com>
2019-11-01 09:15:41 -0700

    Add two tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import Display from './components/Display';
+
+test('Renders without crashing', () => {
+  rtl.render(<App />);
 });
+
+test('Display renders data correctly', () => {
+  const doc = rtl.render(<Display data={[
+    { name: "Alex Morgan", country: "United States", searches: 100 },
+    { name: "Wendie Renard", country: "France", searches: 7 }
+  ]} />);
+
+  const name = doc.getByText('Alex Morgan');
+  const country = doc.getByText('United States');
+  const searches = doc.getByText('100');
+
+  expect(name).toBeInTheDocument();
+  expect(country).toBeInTheDocument();
+  expect(searches).toBeInTheDocument();
+})
+
