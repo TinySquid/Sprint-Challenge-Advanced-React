1b27b92ffc02554e2631074d8f2f13ffb9fe1bcc
Michael Nunes <miketnunes@gmail.com>
2019-11-01 08:24:50 -0700

    Get api data into app and log to console

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Display from './components/Display';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
+class App extends React.Component {
+  state = {};
 
-    </div>
-  );
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(response => response.json())
+      .then(data => this.setState({ data }))
+      .catch(error => console.log(error));
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        {this.state ? console.log(this.state) : null}
+      </div>
+    )
+  }
 }
 
 export default App;
